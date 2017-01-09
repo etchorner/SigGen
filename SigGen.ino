5d885ad792ad00b31f219b18979106a4498c565b
etchorner <chris.horner@gmail.com>
2017-01-08 22:25:07 -0800

    Removal of debugging calls

--- SigGen.ino
+++ SigGen.ino
@@ -1,27 +1,27 @@
-/** 
+/**
 
-SigGen.ino - RF Signal Generator Control System
+  SigGen.ino - RF Signal Generator Control System
 
-Copyright (C) 2015 Christopher M. Horner, chris.horner@gmail.com
-p
-This program is free software : you can redistribute it and / or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  Copyright (C) 2015 Christopher M. Horner, chris.horner@gmail.com
+  p
+  This program is free software : you can redistribute it and / or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program.If not, see <http://www.gnu.org/licenses/>.
 
 **/
 
 //#include <FastIO.h>
 #include <Rotary.h>
-#include <Wire.h> 
+#include <Wire.h>
 #include <si5351.h>
 #include <LiquidCrystal_I2C.h>
 
@@ -29,9 +29,9 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 const byte i2cAddr = 0x27;
 
 // Rotary Encoder parameter variables
-const byte encPinA = 2;  // pin D2
-const byte encPinB = 3; // pin D3
-const byte encPinSw = 4;  // pin D4
+const byte encPinA = 2;  // Nano pin D2
+const byte encPinB = 3; // Nano pin D3
+const byte encPinSw = 4;  // Nano pin D4
 
 // Variables - cleanup?
 volatile int turnCount = 0;
@@ -55,135 +55,127 @@ Si5351 si5351;
 
 void setup()
 {
-	Serial.begin(9600);
-	
-	// create the ISR for changes on the Rotary Encoder
+  // create the ISR for changes on the Rotary Encoder
+  cli();
   PCICR |= (1 << PCIE2);
   PCMSK2 |= (1 << PCINT18) | (1 << PCINT19);
   sei();
 
-	// set up LCD and blank display
+  // set up LCD and blank display
   lcd.init();
   lcd.init();
-//	lcd.begin(20, 4);
-	lcd.backlight();
-	lcd.home();
+  lcd.backlight();
+  lcd.home();
 
-	// set up I/O pins (NOTE: rotary encoder pins are handled in the Rotary library)
-	pinMode(encPinSw, INPUT_PULLUP);
-Serial.println("done encoder sw");
+  // set up I/O pins (NOTE: rotary encoder pins are handled in the Rotary library)
+  pinMode(encPinSw, INPUT_PULLUP);
 
-	// setup the si5351 generator
-	si5351.init(SI5351_CRYSTAL_LOAD_10PF, 0, 0);
-	si5351.set_pll(SI5351_PLL_FIXED, SI5351_PLLA);
-	si5351.drive_strength(SI5351_CLK0, SI5351_DRIVE_8MA);
-	si5351.set_freq(freq, SI5351_CLK0);
+  // setup the si5351 generator
+  si5351.init(SI5351_CRYSTAL_LOAD_10PF, 0, 0);  // 10pf for the Adafruit si5351 modules, uses ABM8G-25.000MHZ-B4Y-T or ABM8G-25.000MHZ-4Y-T3
+  si5351.drive_strength(SI5351_CLK0, SI5351_DRIVE_8MA);
+  si5351.set_freq(freq, SI5351_CLK0);
 
-	// generate/terminate signals
-	si5351.output_enable(SI5351_CLK0, HIGH);  // turn on desired output
-	si5351.set_clock_pwr(SI5351_CLK1, LOW);  // disable unused outputs
-	si5351.set_clock_pwr(SI5351_CLK2, LOW);  // disable unused outputs
-Serial.println("done with dds");
+  // generate/terminate signals
+  si5351.output_enable(SI5351_CLK0, HIGH);  // turn on desired output
+  si5351.set_clock_pwr(SI5351_CLK1, LOW);  // disable unused outputs
+  si5351.set_clock_pwr(SI5351_CLK2, LOW);  // disable unused outputs
 
-	// test display setup  
-	lcd.print("SigGen v0.5 CLK0");
-	lcd.setCursor(0, 1);       // go to the 2nd line  
-	lcd.print("Drive: 8 mA");
-	lcd.setCursor(0, 2);        // go to the third line  
-	lcd.print("Tune Rate : ");; // start data at (10,2)
-	lcd.print(tuneRate[dfindex]);
-	lcd.setCursor(0, 3); // got to 4th line
-	changeFreq();
-	lcd.setCursor(pos, 3);
-	lcd.cursor();
- Serial.println("end setup");
+  // test display setup
+  lcd.print("SigGen v1.0 CLK0");
+  lcd.setCursor(0, 1);       // go to the 2nd line
+  lcd.print("Drive: 8 mA");
+  lcd.setCursor(0, 2);        // go to the third line
+  lcd.print("Tune Rate : ");; // start data at (10,2)
+  lcd.print(tuneRate[dfindex]);
+  lcd.setCursor(0, 3); // go to 4th line
+  changeFreq();
+  lcd.setCursor(pos, 3);
+  lcd.cursor();
 }
 
 void loop()
 {
-	/*
-	TODO: Fix debouncing a la http://arduino.cc/en/Tutorial/Debounce
-	or maybe fix in hardware with some capacitors.
-	*/
+  /*
+    TODO: Fix debouncing a la http://arduino.cc/en/Tutorial/Debounce
+    or maybe fix in hardware with some capacitors.
+  */
 
-	// handle rotary encoder axial switch in the main code.
-	encSwitchState = digitalRead(encPinSw);
- Serial.println("btn");
-	if (encSwitchState == LOW && encLastSwitchState == false)
-	{
-		// decrement the frequency change index for each click
-		dfindex--;
+  // handle rotary encoder axial switch in the main code loop.
+  encSwitchState = digitalRead(encPinSw);
+  if (encSwitchState == LOW && encLastSwitchState == false)
+  {
+    // decrement the frequency change index for each click
+    dfindex--;
 
-		// wrap around from end to beginning
-		if (dfindex < 0)
-			dfindex = 7;
+    // wrap around from end to beginning
+    if (dfindex < 0)
+      dfindex = 7;
 
-		// cursor position and set the debounce variable.
-		pos = 19 - dfindex;
-		encLastSwitchState = true;
-    Serial.println("btn push");
-    
-		// change tune rate display accordingly
-		lcd.setCursor(12, 2);
-		lcd.print(tuneRate[dfindex]);
-		lcd.setCursor(pos, 3);
-	}
+    // cursor position and set the debounce variable.
+    pos = 19 - dfindex;
+    encLastSwitchState = true;
 
-	if (encSwitchState == HIGH && encLastSwitchState == true)
-	{
-		// unset the debounce variable
-		encLastSwitchState = false;
-	} // end of encoder switch handling
+    // change tune rate display accordingly
+    lcd.setCursor(12, 2);
+    lcd.print(tuneRate[dfindex]);
+    lcd.setCursor(pos, 3);
+  }
 
-	// update Rotary Encoder position (pos. #s are right turnCount, neg. #s are left turnCount.
-	if (turnCount != 0)
-	{
-		freq = freq + (turnCount * deltaF[dfindex]);
-		changeFreq();
-		turnCount = 0; //reset the counter
-	}
+  if (encSwitchState == HIGH && encLastSwitchState == true)
+  {
+    // unset the debounce variable
+    encLastSwitchState = false;
+  } // end of encoder switch handling
 
-	// band sweeper code
-	//while (freq < stop)
-	//{
-	//	freq = freq + (turnCount * deltaF[dfindex]);
-	//	changeFreq();
-	//	turnCount++;
-	//	delay(500);
-	//}
+  // update Rotary Encoder position (pos. #s are right turnCount, neg. #s are left turnCount.
+  if (turnCount != 0)
+  {
+    freq = freq + (turnCount * deltaF[dfindex]);
+    changeFreq();
+    turnCount = 0; //reset the counter
+  }
+
+  // band sweeper code
+  //while (freq < stop)
+  //{
+  //	freq = freq + (turnCount * deltaF[dfindex]);
+  //	changeFreq();
+  //	turnCount++;
+  //	delay(500);
+  //}
 }
 
 void changeFreq()
 {
-	// change the output first
-	si5351.set_freq(freq, SI5351_CLK0);
-	String freqStr = String(freq, DEC);
+  // change the output first
+  si5351.set_freq(freq, SI5351_CLK0);
+  String freqStr = String(freq, DEC);
 
-	// blank the line
-	lcd.setCursor(12, 3);
-	lcd.print("       ");
-	lcd.setCursor(0, 3);
-	lcd.print("Freq: ");
+  // blank the line
+  lcd.setCursor(12, 3);
+  lcd.print("       ");
+  lcd.setCursor(0, 3);
+  lcd.print("Freq: ");
 
-	// handle the various sizes of "freq"
-	if (freq >= 10000000){
-		lcd.setCursor(12, 3);
-		lcd.print(freq);
-	}
-	else {
-		if (freq < 1000000){
-			lcd.setCursor(14, 3);
-			lcd.print(freq);
-		}
-		else {
-			lcd.setCursor(13, 3);
-			lcd.print(freq);
-		}
-	}
+  // handle the various sizes of "freq"
+  if (freq >= 10000000) {
+    lcd.setCursor(12, 3);
+    lcd.print(freq);
+  }
+  else {
+    if (freq < 1000000) {
+      lcd.setCursor(14, 3);
+      lcd.print(freq);
+    }
+    else {
+      lcd.setCursor(13, 3);
+      lcd.print(freq);
+    }
+  }
 
-	// establish the cursor position
-	pos = 19 - dfindex;
-	lcd.setCursor(pos, 3);
+  // establish the cursor position
+  pos = 19 - dfindex;
+  lcd.setCursor(pos, 3);
 
 }
 
@@ -196,10 +188,8 @@ ISR(PCINT2_vect) {
   }
   else if (result == DIR_CW) {
     turnCount++;
-    Serial.println("ClockWise");
   }
   else if (result == DIR_CCW) {
     turnCount--;
-    Serial.println("CounterClockWise");
   }
 }
